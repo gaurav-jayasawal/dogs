eb7de19694d7bb994d0e37bdd576e24c936868f8
Gaurav Jayasawal <gaurav.jayasawal@appian.com>
2020-04-20 19:13:03 -0400

    Setup viewmodel and recyclerView

--- app/src/main/java/com/example/dogs/view/DetailFragment.kt
+++ app/src/main/java/com/example/dogs/view/DetailFragment.kt
@@ -1,16 +1,15 @@
 package com.example.dogs.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
-
+import androidx.fragment.app.Fragment
 import com.example.dogs.R
-import kotlinx.android.synthetic.main.fragment_detail.*
 
 class DetailFragment : Fragment() {
+
+    private var dogUuid = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,10 +21,11 @@ class DetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        buttonList.setOnClickListener {
-            val action = DetailFragmentDirections.actionListFragment()
-            Navigation.findNavController(it).navigate(action)
-        }
+
+        arguments?.let {
+            dogUuid = DetailFragmentArgs.fromBundle(it).dogUuid
+         }
+
     }
 
 }
--- app/src/main/java/com/example/dogs/view/ListFragment.kt
+++ app/src/main/java/com/example/dogs/view/ListFragment.kt
@@ -1,31 +1,68 @@
 package com.example.dogs.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
-
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.dogs.R
+import com.example.dogs.viewmodel.ListViewModel
 import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment() {
+
+    private lateinit var viewModel: ListViewModel
+    private val dogsListAdapter = DogsListAdapter(arrayListOf())
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        buttonDetails.setOnClickListener {
-            val action = ListFragmentDirections.actionDetailFragment()
-            Navigation.findNavController(it).navigate(action)
+
+        viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+
+        dogsList.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = dogsListAdapter
         }
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.dogs.observe(viewLifecycleOwner, Observer { dogs ->
+            dogs?.let {
+                dogsList.visibility = View.VISIBLE
+                dogsListAdapter.updateDogList(dogs)
+            }
+        })
+
+        viewModel.dogsLoadError.observe(viewLifecycleOwner, Observer { isError ->
+            isError?.let {
+                list_error.visibility = if(it) View.VISIBLE else View.GONE
+            }
+        })
+
+        viewModel.loading.observe(viewLifecycleOwner, Observer { isLoading ->
+            isLoading?.let {
+                if (it) {
+                    loading_view.visibility = View.VISIBLE
+                    list_error.visibility = View.GONE
+                    dogsList.visibility = View.GONE
+                } else {
+                    loading_view.visibility = View.GONE
+                }
+            }
+        })
     }
 
 }
